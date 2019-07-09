78e9c6f83ae15dc91481353156b61e11ccba97d6
George Gabolaev <g.gabolaev@corp.mail.ru>
2019-07-09 07:26:05 +0300

    added onload

--- js/scripts.js
+++ js/scripts.js
@@ -21,102 +21,104 @@ function renderTypeWriterText(text, selector, speed, resolve, index = 0) {
     }
 }
 
-const hello = document.createElement("div");
-hello.className = "hello";
-document.body.appendChild(hello);
+window.onload = () => {
+    const hello = document.createElement("div");
+    hello.className = "hello";
+    document.body.appendChild(hello);
 
-let helloRender = new Promise((resolve, _) => {
-    renderTypeWriterText(
-        text = "i am george!",
-        selector = ".hello",
-        speed = TYPING_SPEED,
-        resolve,
-    );
-});
-
-
-helloRender.then(() => {
-    const developer = document.createElement("div");
-    developer.className = "developer";
-    document.body.appendChild(developer)
-
-    let developerRender = new Promise((resolve, _) => {
-        setTimeout(
-            renderTypeWriterText,
-            speed = 1000,
-            text = "backend developer",
-            selector = ".developer",
-            speed = -20,
+    let helloRender = new Promise((resolve, _) => {
+        renderTypeWriterText(
+            text = "i am george!",
+            selector = ".hello",
+            speed = TYPING_SPEED,
             resolve,
         );
     });
 
-    developerRender.then(() => {
-        const mail_logo = document.createElement("img");
-        mail_logo.src="static/mrg_red.png";
-        mail_logo.className = "mail_logo";
-        developer.appendChild(mail_logo);
 
-        const aboutMe = document.createElement("div");
-        aboutMe.className = "about-me";
-        document.body.appendChild(aboutMe);
+    helloRender.then(() => {
+        const developer = document.createElement("div");
+        developer.className = "developer";
+        document.body.appendChild(developer)
 
-
-        let aboutRender = new Promise((resolve, _) => {
+        let developerRender = new Promise((resolve, _) => {
             setTimeout(
                 renderTypeWriterText,
-                speed = 500,
-                text = "showing signs of life at",
-                selector = ".about-me",
-                speed = TYPING_SPEED,
+                speed = 1000,
+                text = "backend developer",
+                selector = ".developer",
+                speed = -20,
                 resolve,
             );
         });
 
-        aboutRender.then(() => {
+        developerRender.then(() => {
+            const mail_logo = document.createElement("img");
+            mail_logo.src="static/mrg_red.png";
+            mail_logo.className = "mail_logo";
+            developer.appendChild(mail_logo);
 
-            const linksMapping = [
-                ["static/tg.png", "tg://resolve?domain=gabolaev"],
-                ["static/ln.png", "https://www.linkedin.com/in/gabolaev"],
-                ["static/cv.png", "https://visualcv.com/gabolaev"],
-                ["static/gh.png", "https://github.com/gabolaev"],
-                ["static/ig.png", "https://www.instagram.com/gabolaev"],
-                ["static/tw.png", "https://twitter.com/georgegabolaev"],
-                ["static/vk.png", "https://vk.com/gabolaev"],
-            ];
+            const aboutMe = document.createElement("div");
+            aboutMe.className = "about-me";
+            document.body.appendChild(aboutMe);
 
-            const links = document.createElement("div");
-            links.className = "links";
-            document.body.appendChild(links);
 
-            let linksRender = new Promise((resolve, _) => {
-                function recursive(index = 0) {
-                    if (index != linksMapping.length) {
-                        links.innerHTML +=
-                            `<a href="${linksMapping[index][1]}"><img src="${linksMapping[index][0]}"></a>`;
-                        setTimeout(recursive, 120, ++index);
-                    } else {
-                        resolve();
-                    }
-                };
-                recursive();
-            });
-
-            linksRender.then(() => {
-                const writeMe = document.createElement("a");
-                writeMe.className = "write-me";
-                writeMe.href = "mailto:gabolaev98@gmail.com";
-                document.body.appendChild(writeMe);
-
+            let aboutRender = new Promise((resolve, _) => {
                 setTimeout(
                     renderTypeWriterText,
-                    speed = 300,
-                    text = "want to tell me something?",
-                    selector = ".write-me",
+                    speed = 500,
+                    text = "showing signs of life at",
+                    selector = ".about-me",
                     speed = TYPING_SPEED,
-                    () => { },
+                    resolve,
                 );
+            });
+
+            aboutRender.then(() => {
+
+                const linksMapping = [
+                    ["static/tg.png", "tg://resolve?domain=gabolaev"],
+                    ["static/ln.png", "https://www.linkedin.com/in/gabolaev"],
+                    ["static/cv.png", "https://visualcv.com/gabolaev"],
+                    ["static/gh.png", "https://github.com/gabolaev"],
+                    ["static/ig.png", "https://www.instagram.com/gabolaev"],
+                    ["static/tw.png", "https://twitter.com/georgegabolaev"],
+                    ["static/vk.png", "https://vk.com/gabolaev"],
+                ];
+
+                const links = document.createElement("div");
+                links.className = "links";
+                document.body.appendChild(links);
+
+                let linksRender = new Promise((resolve, _) => {
+                    function recursive(index = 0) {
+                        if (index != linksMapping.length) {
+                            links.innerHTML +=
+                                `<a href="${linksMapping[index][1]}"><img src="${linksMapping[index][0]}"></a>`;
+                            setTimeout(recursive, 120, ++index);
+                        } else {
+                            resolve();
+                        }
+                    };
+                    recursive();
+                });
+
+                linksRender.then(() => {
+                    const writeMe = document.createElement("a");
+                    writeMe.className = "write-me";
+                    writeMe.href = "mailto:gabolaev98@gmail.com";
+                    document.body.appendChild(writeMe);
+
+                    setTimeout(
+                        renderTypeWriterText,
+                        speed = 300,
+                        text = "want to tell me something?",
+                        selector = ".write-me",
+                        speed = TYPING_SPEED,
+                        () => { },
+                    );
+                });
             });
         });
     });
-});
+}
