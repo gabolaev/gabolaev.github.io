1e3cbf83974a83d8e3d311dff382e54cd767ff5f
silvercase <serebrennikov.oleg.p@gmail.com>
2022-04-17 13:28:18 +0300

    Added blinking cursor, changed delays between texts, typing speed.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-const TYPING_SPEED = 86
+const TYPING_SPEED = 70
 const DEFAULT_DELAY = 100
 
 function randomInRange(start, end) {
     return Math.floor(Math.random() * (end - start + 1) + start)
 }
 
+function removeCursor() {
+    const curs = document.getElementsByClassName("cursor");
+    while (curs.length > 0) {
+        curs[0].parentNode.removeChild(curs[0]);
+    }
+}
+
 function renderTypeWriterText(text, element, resolve, index = 0) {
+    removeCursor()
     if (index !== text.length) {
-        element.innerHTML += text.charAt(index)
+        element.innerHTML += text.charAt(index);
+        createCursor(element);
         setTimeout(
             renderTypeWriterText, TYPING_SPEED + randomInRange(-5, 3),
             text, element, resolve, ++index,
@@ -25,7 +34,7 @@ function renderText(element, resolve, text, delay) {
 }
 
 function run(...stages) {
-    let wait = stages[0]()
+    let wait = stages[0]();
     if (stages.length !== 1) {
         wait.then(() => {
             run(...stages.slice(1))
@@ -33,10 +42,12 @@ function run(...stages) {
     }
 }
 
-function prepareStageElement(parentClassName, className, elementType) {
-    var element = document.createElement(elementType)
+function prepareStageElement(parentClassName, className, elementType, withCursor = true) {
+    const element = document.createElement(elementType);
     element.className = className;
-
+    if (withCursor) {
+        createCursor(element);
+    }
     (
         document.querySelector("." + parentClassName) ||
         document.body
@@ -46,26 +57,34 @@ function prepareStageElement(parentClassName, className, elementType) {
 
 function text(parentClassName, className, elementType, text, delay = DEFAULT_DELAY) {
     return () => {
-        element = prepareStageElement(parentClassName, className, elementType)
+        let element = prepareStageElement(parentClassName, className, elementType)
         return new Promise((resolve, _) => {
             renderText(element, resolve, text, delay)
         })
     }
 }
 
-function custom(parentClassName, className, elementType, f) {
+function createCursor(refElement) {
+    const cursor = refElement.insertBefore(document.createElement("span"), null);
+    cursor.id = "cursor";
+    cursor.className = "cursor";
+    cursor.style.fontSize = refElement.style.fontSize;
+    cursor.innerHTML = "|";
+}
+
+function custom(parentClassName, className, elementType, f, withCursor = true) {
     return () => {
-        return new Promise(f(prepareStageElement(parentClassName, className, elementType)))
+        return new Promise(f(prepareStageElement(parentClassName, className, elementType, withCursor)))
     }
 }
 
 run(
-    text("body", "hello", "div", "hello"),
-    text("body", "engineer", "div", "i am a software engineer at ", 1000),
-    text("engineer", "tinkoff", "span", "Tinkoff Investments", 10),
-    text("body", "ninja", "div", "also i am 忍者 dev at "),
-    text("ninja", "kuji", "span", "KUJI Podcast", 100),
-    text("body", "about-me", "div", "i left my digital footprints here"),
+    text("body", "hello", "div", "hello,", 2000),
+    text("body", "engineer", "div", "i am a software engineer at ", 700),
+    text("engineer", "tinkoff", "span", "Tinkoff Investments", 250),
+    text("body", "ninja", "div", "also i am 忍者 dev at ", 1000),
+    text("ninja", "kuji", "span", "KUJI Podcast", 250),
+    text("body", "about-me", "div", "i left my digital footprints here", 700),
     custom("body", "links", "div",
         (element) => {
             return (resolve, _) => {
@@ -85,19 +104,25 @@ run(
                         return
                     }
                     element.innerHTML += `<a href="${linksMapping[index][1]}"><img src="${linksMapping[index][0]}"></a>`
-                    setTimeout(renderLink, 120, ++index)
+                    setTimeout(renderLink, 0, ++index)
                 }
+
                 renderLink()
             }
         },
+        false // No caret needed here
     ),
     custom("body", "email-me", "a",
         (element) => {
             return (resolve, _) => {
                 element.href = "mailto:george@gabolaev.com";
-                renderText(element, resolve, "email me")
+                renderText(element, resolve, "email me", 2000)
             }
         },
     ),
-    text("body", "btw", "div", "btw, my name is george"),
+    text("body", "btw", "div", "btw, my name is george", 10000),
+    text("body", "btw", "div", "so, what are you waiting for?", 10000),
+    text("body", "btw", "div", "there is nothing more to see here...", 20000),
+    text("body", "btw", "div", "i swear :)", 1000),
+    text("body", "btw", "div", "i hope you have a great day!", 1000),
 )
