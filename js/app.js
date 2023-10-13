c115c27a23593bc4f36e0edfc5cad83c6871fd1c
George Gabolaev <gabolaev@talon.one>
2023-10-13 03:48:06 +0200

    refactored

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-const TYPING_SPEED = 86
-const DEFAULT_DELAY = 100
+const TYPING_SPEED = 63;
+const DEFAULT_DELAY = 100;
 
 function randomInRange(start, end) {
     return Math.floor(Math.random() * (end - start + 1) + start)
 }
 
 function createCursor(refElement) {
-    const cursor = refElement.insertBefore(document.createElement("span"), null);
-    cursor.id = "cursor";
-    cursor.className = "cursor";
+    const cursor = refElement.insertBefore(document.createElement('span'), null);
+    cursor.id = 'cursor';
+    cursor.className = 'cursor';
     cursor.style.fontSize = refElement.style.fontSize;
-    cursor.innerHTML = "|";
+    cursor.innerHTML = '|';
 }
 
 function removeCursor() {
-    const curs = document.getElementsByClassName("cursor");
+    const curs = document.getElementsByClassName('cursor');
     while (curs.length > 0) {
         curs[0].parentNode.removeChild(curs[0]);
     }
@@ -53,107 +53,179 @@ function run(...stages) {
     }
 }
 
-function prepareStageElement(parentClassName, className, elementType) {
+let parentsHistory = {
+    'body': 'body',
+    'prev': 'body',
+    'exPrev': 'body',
+}
+
+let Base = {
+    Body: 'body',
+    Prev: 'prev',
+    ExPrev: 'exPrev',
+}
+
+function prepareStageElement(baseControl, className, elementType) {
+    let parentClassName = parentsHistory[baseControl];
+    parentsHistory.exPrev = parentsHistory.prev;
+    parentsHistory.prev = className;
+
     const element = document.createElement(elementType);
     element.className = className;
 
+    let parents = document.querySelectorAll('.' + parentClassName.replace(' ', '.'));
+
     (
-        document.querySelector("." + parentClassName) ||
+        parents[parents.length - 1] ||
         document.body
     ).appendChild(element)
     return element
 }
 
-function text(parentClassName, className, elementType, text, delay = DEFAULT_DELAY) {
+
+function text(baseControl, className, elementType, text, delay = DEFAULT_DELAY) {
     return () => {
-        let element = prepareStageElement(parentClassName, className, elementType)
-        return new Promise((resolve, _) => {
-            renderText(element, resolve, text, delay)
+        return new Promise((resolve) => {
+            renderText(prepareStageElement(baseControl, className, elementType), resolve, text, delay)
         })
     }
 }
 
-function custom(parentClassName, className, elementType, f) {
+function enter() {
+    return text(Base.Body, 'skip', 'br', '', 0);
+}
+
+function comma(baseControl) {
+    return text(baseControl, 'comma', 'span', ', ', 200);
+}
+
+function period(baseControl) {
+    return text(baseControl, 'period', 'span', '. ', 200);
+}
+
+function custom(baseControl, className, elementType, f) {
     return () => {
-        let element = prepareStageElement(parentClassName, className, elementType)
-        return new Promise(f(element))
+        return new Promise(f(prepareStageElement(baseControl, className, elementType)))
     }
 }
 
 run(
-    text("body", "hello", "div", "приве", 2500),
-    custom("hello", "waiting", "span", (_) => {
-        return (resolve, _) => {
+    text(Base.Body, 'hello', 'div', 'приве', 1000),
+    custom(Base.Body, 'action', 'span', () => {
+        return (resolve) => {
             document.querySelector('.hello').remove();
-            resolve()
+            resolve();
         }
     }),
-    text("body", "hello", "div", "hello", 1100),
-    custom("hello", "waiting", "span", (_) => {
-        return (resolve, _) => {
-            setTimeout(resolve, 1500)
+    text(Base.Body, 'hello', 'div', 'hello', 1000),
+    custom(Base.Prev, 'action', 'span', () => {
+        return (resolve) => {
+            setTimeout(resolve, 500);
         }
     }),
-    text("body", "name", "div", "my name is george and i'm a software engineer", 500),
-    text("body", "engineer", "div", "i work at ", 500),
-    text("engineer", "talon", "span", "talon.one", 200),
-    text("talon", "berlin", "span", " in berlin, germany", 100),
-    text("body", "skip", "br", "", 0),
-    text("body", "previously", "div", "previously worked at ", 200),
-    // text("previously", "talon", "span", "T1", 200),
-    // text("talon", "and a", "span", ", ", 200),
-    text("previously", "vk", "span", "vk", 200),
-    text("vk", "and b", "span", ", ", 200),
-    text("and.b", "tinkoff", "span", "tinkoff", 200),
-    text("body", "ninja", "div", "and "),
-    text("ninja", "kuji", "span", "kuji podcast", 200),
-    text("body", "skip", "br", "", 0),
-    text("body", "skip", "br", "", 0),
-    text("body", "about", "div", "you can follow my digital footsteps", 1000),
-    custom("body", "links", "div",
+    text(Base.Body, 'plain', 'div', 'my name is george and i\'m a software engineer.', 100),
+    text(Base.Body, 'plain', 'div', 'i work at ', 500),
+    text(Base.Prev, 'talon', 'span', 'talon.one', 200),
+    text(Base.ExPrev, 'plain', 'span', ' in berlin, germany', 100),
+    period(Base.Prev),
+    enter(),
+    text(Base.Body, 'plain', 'div', 'in the past, i worked at ', 200),
+    text(Base.Prev, 'vk', 'span', 'vk', 200),
+    comma(Base.ExPrev),
+    text(Base.Prev, 'tinkoff', 'span', 'tinkoff', 200),
+    // comma(Base.ExPrev),
+    // text(Base.Prev, 'talon', 'span', 'talon.one', 200),
+    text(Base.Body, 'plain', 'div', 'and '),
+    text(Base.Prev, 'kuji', 'span', 'kuji podcast', 200),
+    period(Base.ExPrev),
+    enter(),
+    text(Base.Body, 'plain', 'div', 'you can find me on ', 1000),
+    custom(Base.Prev, 'link', 'a',
         (element) => {
-            return (resolve, _) => {
-                removeCursor()
-                const linksMapping = [
-                    ["static/tg.png", "https://t.me/gabolaev"],
-                    ["static/ln.png", "https://www.linkedin.com/in/gabolaev"],
-                    ["static/gh.png", "https://github.com/gabolaev"],
-                    ["static/sp.png", "https://open.spotify.com/user/gabolaev"],
-                    ["static/lb.png", "https://letterboxd.com/gabolaev"],
-                    ["static/ig.png", "https://www.instagram.com/gabolaev"],
-                    ["static/fb.png", "https://fb.com/gabolaev"],
-                ]
-
-                for (i in linksMapping) {
-                    element.innerHTML += `<a style="display: none" href="${linksMapping[i][1]}"><img src="${linksMapping[i][0]}"></a>`
-                }
-
-                function renderLink(index = 0) {
-                    if (index === linksMapping.length) {
-                        resolve()
-                        return
-                    }
-                    element.childNodes[index].style.display = "unset"
-                    setTimeout(renderLink, 200, ++index)
-                }
-
-                renderLink()
+            return (resolve) => {
+                element.href = 'https://t.me/gabolaev';
+                renderText(element, resolve, 'telegram', 100);
             }
         },
     ),
-    text("body", "email-me", "div", "or hit me up with an "),
-    custom("email-me", "email-link", "a",
+    comma(Base.ExPrev),
+    custom(Base.Prev, 'link', 'a',
         (element) => {
-            return (resolve, _) => {
-                element.href = "mailto:george@gabolaev.com";
-                renderText(element, resolve, "email", 100)
+            return (resolve) => {
+                element.href = 'https://www.linkedin.com/in/gabolaev';
+                renderText(element, resolve, 'linkedin', 100);
             }
         },
     ),
-    text("body", "enter", "br", "", 450),
-    text("body", "enter", "p", "", 450),
-    text("body", "enter", "br", "", 250),
-    text("body", "enter", "p", "", 250),
-    text("body", "enter", "br", "", 250),
-    text("body", "enter", "p", "🧤", 10000),
+    comma(Base.ExPrev),
+    text(Base.Prev, 'plain', 'div', '', 100),
+    custom(Base.Prev, 'link', 'a',
+        (element) => {
+            return (resolve) => {
+                element.href = 'https://github.com/gabolaev';
+                renderText(element, resolve, 'github', 100);
+            }
+        },
+    ),
+    comma(Base.ExPrev),
+    custom(Base.Prev, 'link', 'a',
+        (element) => {
+            return (resolve) => {
+                element.href = 'https://open.spotify.com/user/gabolaev';
+                renderText(element, resolve, 'spotify', 100);
+            }
+        },
+    ),
+    comma(Base.ExPrev),
+    custom(Base.Prev, 'link', 'a',
+        (element) => {
+            return (resolve) => {
+                element.href = 'https://www.instagram.com/gabolaev';
+                renderText(element, resolve, 'instagram', 100);
+            }
+        },
+    ),
+
+    comma(Base.ExPrev),
+    custom(Base.Prev, 'link', 'a',
+        (element) => {
+            return (resolve) => {
+                element.href = 'https://letterboxd.com/gabolaev';
+                renderText(element, resolve, 'letterboxd', 100);
+            }
+        },
+    ),
+    text(Base.ExPrev, 'plain', 'span', ' and '),
+    custom(Base.Prev, 'link twitter', 'a',
+        (element) => {
+            return (resolve) => {
+                element.href = 'https://x.com/rugbeehead';
+                renderText(element, resolve, 'twitter', 100);
+            }
+        },
+    ),
+    period(Base.ExPrev),
+    custom(Base.Body, 'action', 'span', () => {
+        return (resolve) => {
+            document.querySelector('.twitter').innerHTML = "𝕏";
+            resolve();
+        }
+    }),
+    enter(),
+    text(Base.Body, 'plain', 'div', 'or hit me up with an '),
+    custom(Base.Prev, 'link', 'a',
+        (element) => {
+            return (resolve) => {
+                element.href = 'mailto:george@gabolaev.com';
+                renderText(element, resolve, 'email', 100);
+            }
+        },
+    ),
+    period(Base.ExPrev),
+    text(Base.Body, 'plain', 'br', '', 450),
+    text(Base.Body, 'plain', 'p', '', 450),
+    text(Base.Body, 'plain', 'br', '', 250),
+    text(Base.Body, 'plain', 'p', '', 250),
+    text(Base.Body, 'plain', 'br', '', 250),
+    text(Base.Body, 'plain', 'p', '🧤', 10000),
 )
